d2b9022014b50c43f63daf4b51918ee17c47555e
Antonio Ricardo <antonio.eschola@gmail.com>
2023-03-14 22:59:21 -0400

    feat: add fourh layer components and flow

--- src/components/products/ChaptersList.tsx
+++ src/components/products/ChaptersList.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { MouseEventHandler, useState } from 'react';
+import axios from 'axios';
 
 import { Tooltip, TooltipTrigger, TooltipContent } from '~/core/ui/Tooltip';
-
 import IconButton from '~/core/ui/IconButton';
 
 import {
   ArrowUpRightIcon,
   ArrowTopRightOnSquareIcon,
 } from '@heroicons/react/24/outline';
+
+import toaster from 'react-hot-toast';
 
 interface ChapterListProps {
   chapter: string;
@@ -16,6 +18,29 @@ interface ChapterListProps {
 
 const ChapterList = ({ chapter, index }: ChapterListProps) => {
   const chapterTitle = chapter.replace(/^[0-9]+\. /, '');
+  const [generatedContent, setGeneratedContent] = useState('');
+
+  const generateContent: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.stopPropagation();
+    try {
+      const response = await toaster.promise(
+        axios.post('/api/products/fourthLayer', {
+          chapterTitle: chapterTitle,
+          ebookTitle: 'ebookTitle',
+          ebookNiche: 'niche',
+        }),
+        {
+          success: `Great news! Your content was successfully created.`,
+          loading: `Hold on, I'm generating the content for your chapter...`,
+          error: `Oops! Something went wrong while generating the content to your infoproduct. Please try again later.`,
+        }
+      );
+      console.log(`Success! Message: ${response.data.content}`);
+      setGeneratedContent(response.data.content);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div
@@ -24,14 +49,16 @@ const ChapterList = ({ chapter, index }: ChapterListProps) => {
       }
       key={index}
     >
-      <div key={index} className={'flex items-center space-x-4'}>
-        <div key={index} className={'flex flex-1 flex-col space-y-0.5'}>
-          <li key={index}>{chapterTitle.replace(/^[0-9]+ - [0-9]+\. /, '')}</li>
+      <div className={'flex items-center space-x-4'} key={index}>
+        <div className={'flex flex-1 flex-col space-y-0.5'}>
+          <li>{chapterTitle.replace(/^[0-9]+ - [0-9]+\. /, '')}</li>
+          {/* show generated content if exists */}
+          {generatedContent && <p>{generatedContent}</p>}
         </div>
         <div className={'flex justify-end'}>
           {!chapter && (
             <Tooltip>
-              <TooltipTrigger asChild>
+              <TooltipTrigger onClick={generateContent} asChild>
                 <IconButton>
                   <ArrowTopRightOnSquareIcon className={'h-5 text-blue-500'} />
                 </IconButton>
@@ -40,7 +67,7 @@ const ChapterList = ({ chapter, index }: ChapterListProps) => {
             </Tooltip>
           )}
           <Tooltip>
-            <TooltipTrigger asChild>
+            <TooltipTrigger onClick={generateContent} asChild>
               <IconButton>
                 <ArrowUpRightIcon className={'h-5 text-indigo-900'} />
               </IconButton>
